47d3883d8808f3143c2d8b8694c0f783a59e1679
Patrick José Pereira <patrickelectric@gmail.com>
2020-11-06 17:08:43 -0300

    First version
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ python-project-template/__main__.py
@@ -0,0 +1,11 @@
+"""Caravela Hacker Club Python project template
+"""
+
+
+def main():
+    """Main function for the project"""
+    print('This is just a simple template for python projects.')
+
+
+if __name__ == '__main__':
+    main()
